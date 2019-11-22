4165ec44e4530cf4ad9ac2d6a02d894228f9bbab
vk-service <56194695+vk-service@users.noreply.github.com>
2019-11-22 16:43:21 +0300

    Add files via upload

---
+++ precache-manifest.a540420a80170a6abd890293934e69d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "20f5702860d2db785973",
+    "url": "./static/js/main.862fda41.chunk.js"
+  },
+  {
+    "revision": "73deb0d6b7ae8b85fa07",
+    "url": "./static/js/2.eeb3a0b3.chunk.js"
+  },
+  {
+    "revision": "20f5702860d2db785973",
+    "url": "./static/css/main.7e897168.chunk.css"
+  },
+  {
+    "revision": "73deb0d6b7ae8b85fa07",
+    "url": "./static/css/2.f22c59e1.chunk.css"
+  },
+  {
+    "revision": "f6262857c776bb6b056cef133e860909",
+    "url": "./index.html"
+  }
+];
